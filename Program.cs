1ee36a759ecbe3ae69e05fdbd86d9d781772a965
PaulTh <paul.thalhammer@yahoo.com>
2025-09-22 08:13:55 +0200

    fast Forward Merge incoming

--- Program.cs
+++ Program.cs
@@ -10,3 +10,5 @@ Console.WriteLine("-------------------------------");
 double d = 1.1, e = 2.2, f = 3.3;
 Console.WriteLine("ein paar double values");
 Console.WriteLine($"d: {d}");
+
+Console.WriteLine("ACHTUNG:FastForward Merge");
